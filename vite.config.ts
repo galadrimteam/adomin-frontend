c6f4ff9606d366577e01d911d5ca591465c085c2
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-11-03 16:04:17 +0100

    feat: proxy images for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 3000,
+    proxy: {
+      "/uploads": {
+        target: "http://127.0.0.1:3333",
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react()],
 });
