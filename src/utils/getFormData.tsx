034d5e83459f9e463e77efb5d89e0497bdccd22b
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-09-18 16:02:34 +0200

    fix: nullable date/datetime bug

--- src/utils/getFormData.tsx
+++ src/utils/getFormData.tsx
@@ -30,10 +30,10 @@ const appendData = (formData: FormData, field: ModelField, data: any) => {
     if (file === null && !dataToAppend.shouldDestroy) return;
     return formData.append(field.name, file ?? "");
   }
-  if (field.adomin.type === "date" && field.adomin.subType === "date") {
+  if (field.adomin.type === "date" && field.adomin.subType === "date" && dataToAppend) {
     return formData.append(field.name, format(dataToAppend, "yyyy-MM-dd"));
   }
-  if (field.adomin.type === "date" && field.adomin.subType === "datetime") {
+  if (field.adomin.type === "date" && field.adomin.subType === "datetime" && dataToAppend) {
     return formData.append(field.name, dataToAppend.toISOString());
   }
 
