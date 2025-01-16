43954ac2aebf08ec530e9452eebc909e85e28009
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2025-01-16 17:56:21 +0100

    feat: better HasManyRelationCell/ManyToManyRelationCell

--- src/components/cells/HasManyRelationCell.tsx
+++ src/components/cells/HasManyRelationCell.tsx
@@ -1,14 +1,18 @@
 import { Alert, Box, Chip } from "@mui/material";
 import { MRT_ColumnDef } from "material-react-table";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { getModelPath } from "../../adominPaths";
 import { useModelConfig } from "../../pages/models/ModelConfigContext";
 import type { ModelData } from "../../pages/models/model.types";
 import { getForeignKeyOptionLabel } from "../form/selects/foreignKey/getForeignKeyOptionLabel";
 
+const DEFAULT_LINKS_LIMIT = 9;
+
 export const HasManyRelationCell: MRT_ColumnDef<ModelData>["Cell"] = ({
   cell,
 }) => {
+  const [showAll, setShowAll] = useState(false);
   const columnName = cell.getContext().column.id;
   const cellValue = cell.getValue() as ModelData[];
   const modelConfig = useModelConfig();
@@ -28,9 +32,11 @@ export const HasManyRelationCell: MRT_ColumnDef<ModelData>["Cell"] = ({
     label: getForeignKeyOptionLabel(v, labelFields, labelFieldsSeparator),
   }));
 
+  const valuesToShow = showAll ? values : values.slice(0, DEFAULT_LINKS_LIMIT);
+
   return (
     <Box>
-      {values.map(({ fkValue, label }) => (
+      {valuesToShow.map(({ fkValue, label }) => (
         <Link
           key={fkValue}
           to={getModelPath({
@@ -42,6 +48,26 @@ export const HasManyRelationCell: MRT_ColumnDef<ModelData>["Cell"] = ({
           <Chip label={label} size="small" className="m-1" />
         </Link>
       ))}
+
+      {showAll === false && valuesToShow.length !== values.length && (
+        <Chip
+          label={"+"}
+          size="small"
+          className="m-1"
+          color="info"
+          onClick={() => setShowAll(true)}
+        />
+      )}
+
+      {showAll === true && valuesToShow.length > DEFAULT_LINKS_LIMIT && (
+        <Chip
+          label={"-"}
+          size="small"
+          className="m-1"
+          color="info"
+          onClick={() => setShowAll(false)}
+        />
+      )}
     </Box>
   );
 };
